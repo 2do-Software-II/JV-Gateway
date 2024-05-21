57503986511b4e7e100f74d9a41496d7819998fb
Nahuel Zalazar <zvnahuel63@gmail.com>
2024-05-21 16:55:29 -0400

    add(mongo): Add mongodb

---
+++ src/main/java/com/hotel/hotel/Config/MongoConfig.java
@@ -0,0 +1,16 @@
+package com.hotel.hotel.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import com.mongodb.client.MongoClient;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(mongoClient, "hotel"));
+    }
+}
