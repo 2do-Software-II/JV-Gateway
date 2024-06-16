cd14e77cc2157c553fe956c0eaee3ab895248e1a
Nahuel Zalazar <zvnahuel63@gmail.com>
2024-06-16 19:58:42 -0400

    add(booking): Add booking

--- src/main/java/com/hotel/hotel/Global/TestController.java
+++ src/main/java/com/hotel/hotel/Global/TestController.java
@@ -3,8 +3,11 @@ package com.hotel.hotel.Global;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hotel.hotel.Dtos.CreateBookingDto;
+import com.hotel.hotel.Entities.Booking;
 import com.hotel.hotel.Entities.Room;
 import com.hotel.hotel.Entities.Service;
+import com.hotel.hotel.Services.BookingService;
 import com.hotel.hotel.Services.RoomService;
 import com.hotel.hotel.Services.ServiceService;
 
@@ -15,6 +18,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/test")
@@ -25,6 +30,8 @@ public class TestController {
     private RoomService roomService;
     @Autowired
     private ServiceService serviceService;
+    @Autowired
+    private BookingService bookingService;
 
     @GetMapping("/rooms")
     public ResponseEntity<List<Room>> rooms() {
@@ -34,6 +41,11 @@ public class TestController {
     @GetMapping("/services")
     public ResponseEntity<List<Service>> services() {
         return ResponseEntity.ok(serviceService.getAll());
+    }
+
+    @PostMapping("/booking")
+    public ResponseEntity<Booking> createBooking(@RequestBody CreateBookingDto createBookingDto) {
+        return ResponseEntity.ok(bookingService.create(createBookingDto));
     }
 
 }
