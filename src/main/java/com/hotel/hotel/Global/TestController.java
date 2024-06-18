5be528691400180dfd92a6cd3b6dac1950927b15
Nahuel Zalazar <zvnahuel63@gmail.com>
2024-06-18 14:58:08 -0400

    fix

--- src/main/java/com/hotel/hotel/Global/TestController.java
+++ src/main/java/com/hotel/hotel/Global/TestController.java
@@ -21,6 +21,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class TestController {
 
     @Autowired
